5a0d7f7dfa93c87b288f1d52867652752c89f79a
moehtaroom27 <moehtaroom270895@gmail.com>
2017-11-24 00:03:13 +0700

    0.01

---
+++ app/src/main/java/com/example/asus/invitationcard/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.asus.invitationcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button1);
+
+        // Show toast message when button is clicked
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "I will attend the party", Toast.LENGTH_LONG).show();// Set your own toast  message
+            }
+        });
+
+
+    }
+}
